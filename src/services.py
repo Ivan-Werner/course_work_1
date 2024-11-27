b2d2371cb5e4244bf01f5183a761d8f6118207c6
ivan_kalinov <ikalinov1989@yandex.ru>
2024-11-27 14:19:15 +0400

    Add func simple_search

--- src/services.py
+++ src/services.py
@@ -1,6 +1,7 @@
 import os
 import json
 from http.client import responses
+import re
 
 import requests
 from datetime import datetime, timedelta
@@ -10,4 +11,17 @@ from config import DATA_DIR, operations_path_xlsx
 from dotenv import load_dotenv
 from views import xlsx_reading, numcards_list, main_list
 
-print(xlsx_reading(operations_path_xlsx))
+def simple_search(main_list):
+    """Функция реализует поиск по ключам Категория и Описание"""
+    pattern = input("Найти: ")
+    res = []
+    for i in main_list:
+        if (type(i["Категория"]) == str) and (re.findall(pattern, i["Категория"], flags=re.IGNORECASE)) or (re.findall(pattern, i["Описание"], flags=re.IGNORECASE)):
+            res.append(i)
+    return res
+
+
+if __name__ == '__main__':
+    s = simple_search(main_list)
+    print(s)
+
